ee8a78ae6b033e058e7df55a4725ccbd6c2a37ab
kamalika0363 <kamalikaghora14@gmail.com>
2024-04-28 16:39:45 +0530

    nav

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Drafts',
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
